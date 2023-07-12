ba79426f69eba4c5fc05410e35c7bafddca9773f
beihu235 <105789304+beihu235@users.noreply.github.com>
2023-07-12 22:30:01 +0800

    Add files via upload

--- source/Note.hx
+++ source/Note.hx
@@ -329,7 +329,7 @@ class Note extends FlxSprite
 
 		if (isSustainNote)
 		{
-			animation.addByPrefix('purpleholdend', 'pruple end hold'); // ?????
+			//animation.addByPrefix('purpleholdend', 'pruple end hold'); // ?????
 			animation.addByPrefix(colArray[noteData] + 'holdend', colArray[noteData] + ' hold end');
 			animation.addByPrefix(colArray[noteData] + 'hold', colArray[noteData] + ' hold piece');
 		}
