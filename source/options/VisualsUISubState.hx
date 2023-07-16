1e262f0e219e0e08a3afea921349b62ce67064bf
beihu235 <105789304+beihu235@users.noreply.github.com>
2023-07-16 15:02:16 +0800

    Add files via upload

--- source/options/VisualsUISubState.hx
+++ source/options/VisualsUISubState.hx
@@ -86,6 +86,13 @@ class VisualsUISubState extends BaseOptionsMenu
 			'Time Left',
 			['Time Left', 'Time Elapsed', 'Song Name', 'Disabled']);
 		addOption(option);
+		
+		var option:Option = new Option('GradientColor TimeBar ',
+			"If chacked timebar will have gradient color\nbut maybe will have bug when you change timebar color again",
+			'gradientTimeBar',
+			'bool',
+			true);
+		addOption(option);
 
 		var option:Option = new Option('Flashing Lights',
 			"Uncheck this if you're sensitive to flashing lights!",
