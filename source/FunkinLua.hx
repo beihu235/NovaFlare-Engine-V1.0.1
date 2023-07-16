8ef502c197a2d80ce45b21ffc467f9641ab62803
beihu235 <105789304+beihu235@users.noreply.github.com>
2023-07-16 09:55:23 +0800

    Add files via upload

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -2294,7 +2294,12 @@ class FunkinLua {
 			if (text3 == null) text3 = '';
 			if (text4 == null) text4 = '';
 			if (text5 == null) text5 = '';
-			luaTrace('' + text1 + text2 + text3 + text4 + text5, true, false);
+			
+			if (text2 == '') luaTrace('date1:' + text1, true, false);
+			if (text3 == '') luaTrace('date1:' + text1 + 'date2:' + text2, true, false);
+			if (text4 == '') luaTrace('date1:' + text1 + 'date2:' + text2 + 'date3:' + text3, true, false);
+			if (text5 == '') luaTrace('date1:' + text1 + 'date2:' + text2 + 'date3:' + text3 + 'date4:' + text4, true, false);
+			if (text5 != '')  luaTrace('date1:' + text1 + 'date2:' + text2 + 'date3:' + text3 + 'date4:' + text4 +'date5:' + text5, true, false);
 		});
 		
 		Lua_helper.add_callback(lua, "close", function() {
