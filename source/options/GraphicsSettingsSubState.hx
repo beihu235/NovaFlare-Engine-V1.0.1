0a9bea8cdbf9b74ac0a123eb2bb03e409e2b1df9
beihu235 <105789304+beihu235@users.noreply.github.com>
2023-07-19 12:46:19 +0800

    Add files via upload

--- source/options/GraphicsSettingsSubState.hx
+++ source/options/GraphicsSettingsSubState.hx
@@ -53,12 +53,7 @@ class GraphicsSettingsSubState extends BaseOptionsMenu
 		option.onChange = onChangeAntiAliasing; //Changing onChange is only needed if you want to make a special interaction after it changes the value
 		addOption(option);
 		
-		var option:Option = new Option('Optimize',
-			'If checked, Game will optimize, But you cant use original fnf stage',
-			'optimize',
-			'bool',
-			true);
-		addOption(option);
+		
 
 		var option:Option = new Option('Shaders', //Name
 			'If unchecked, disables shaders.\nIt\'s used for some visual effects, and also CPU intensive for weaker PCs.', //Description
