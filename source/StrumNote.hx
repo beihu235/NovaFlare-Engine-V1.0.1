ed96a2885212c66d426e58184c5f83e2e4217747
beihu235 <105789304+beihu235@users.noreply.github.com>
2023-07-19 18:01:26 +0800

    Add files via upload

--- source/StrumNote.hx
+++ source/StrumNote.hx
@@ -35,7 +35,7 @@ class StrumNote extends FlxSprite
 		super(x, y);
     
         var skin:String = 'NOTE_assets';
-        if (ClientPrefs.ChangeSkin) skin = 'NoteSkin/' + ClientPrefs.NoteSkinName;
+        //if (ClientPrefs.ChangeSkin) skin = 'NoteSkin/' + ClientPrefs.NoteSkinName;
 		
 		if(PlayState.SONG.arrowSkin != null && PlayState.SONG.arrowSkin.length > 1) skin = PlayState.SONG.arrowSkin;
 		texture = skin; //Load texture and anims
