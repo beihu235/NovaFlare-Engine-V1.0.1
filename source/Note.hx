7ad4f0176d0eff6541bfa68abf110dba1674377b
beihu235 <105789304+beihu235@users.noreply.github.com>
2023-07-22 00:46:45 +0800

    Add files via upload

--- source/Note.hx
+++ source/Note.hx
@@ -257,7 +257,7 @@ class Note extends FlxSprite
 		if(texture.length < 1) {
 			skin = PlayState.SONG.arrowSkin;
 			if(skin == null || skin.length < 1) {
-			    if (ClientPrefs.NoteSkin == 'original'){
+			    if (ClientPrefs.NoteSkin != 'original'){
 				skin = 'NoteSkin/' + ClientPrefs.NoteSkin;
 				}
 				else{
