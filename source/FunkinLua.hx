4d117e0eed5d02a7dce72a5fb37d1cdbde74206e
beihu235 <105789304+beihu235@users.noreply.github.com>
2023-07-22 19:46:14 +0800

    Add files via upload

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -1472,7 +1472,7 @@ class FunkinLua {
 				case 'back': key = PlayState.instance.getControl('BACK');
 				case 'pause': key = PlayState.instance.getControl('PAUSE');
 				case 'reset': key = PlayState.instance.getControl('RESET');
-				case 'space': key = PlayState.instance.getControl('SPACE_P');//an extra key for convinience
+				case 'space': key = (PlayState.instance.getControl('SPACE_P') || FlxG.keys.justPressed.SPACE);//an extra key for convinience
 			}
 			return key;
 		});
@@ -1483,7 +1483,7 @@ class FunkinLua {
 				case 'down': key = PlayState.instance.getControl('NOTE_DOWN');
 				case 'up': key = PlayState.instance.getControl('NOTE_UP');
 				case 'right': key = PlayState.instance.getControl('NOTE_RIGHT');
-				case 'space': key = PlayState.instance.getControl('SPACE');//an extra key for convinience
+				case 'space': key = (PlayState.instance.getControl('SPACE') || FlxG.keys.pressed.SPACE);//an extra key for convinience
 			}
 			return key;
 		});
@@ -1494,7 +1494,7 @@ class FunkinLua {
 				case 'down': key = PlayState.instance.getControl('NOTE_DOWN_R');
 				case 'up': key = PlayState.instance.getControl('NOTE_UP_R');
 				case 'right': key = PlayState.instance.getControl('NOTE_RIGHT_R');
-				case 'space': key = PlayState.instance.getControl('SPACE_R');//an extra key for convinience
+				case 'space': key = (PlayState.instance.getControl('SPACE_R') || FlxG.keys.justReleased.SPACE);//an extra key for convinience
 			}
 			return key;
 		});
